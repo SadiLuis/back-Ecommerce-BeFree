e3b1bf4f4bf5ba5a28c195a3576e49fbd1eae7c3
lschacon1988 <lschacon1988@gmail.com>
2023-08-05 15:29:30 -0500

    update structure and database

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ export default{
     port:process.env.NODE_ENV === 'test' ?'http://localhost:1020': process.env.PORT,
     baseURL:  process.env.BASE_URL||'',
     jwtSecret: process.env.JWT_SECRET || 'somesecrettoken',
-    stripeKey: process.env.STRIPE_SERCRET_KEY||"",
+    
     DB:{
         URI: process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/test' : process.env.DB_URI || 'mongodb://localhost:27017/prueba' ,
         
