f80cbd839bfdeec3eb7d37eda157a9348f4dace7
lschacon1988 <lschacon1988@gmail.com>
2023-08-17 07:26:15 -0500

    update tsconfig.json

--- dist/util/const/const.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LENGTH_DESCRIPTION = void 0;
-const LENGTH_DESCRIPTION = 70;
-exports.LENGTH_DESCRIPTION = LENGTH_DESCRIPTION;
